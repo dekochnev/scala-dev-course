e304d327ab7141c5f4132ffb08141430c63fa385
Dmitrii K. <dekochnev@yandex.ru>
2026-06-23 17:29:15 +0300

    homework-03 Collections task (#4)
    * homework-03 Collections task
    
    * homework-03 Collections task +1
    
    * homework-03 Collections task +2
    
    * homework-03 Collections task +3

--- src/main/scala/ru/otus/module1/collectionsHW/06-collections-task.scala
+++ src/main/scala/ru/otus/module1/collectionsHW/06-collections-task.scala
@@ -15,7 +15,12 @@ object collectionsTask {
    *
    * **/
   def capitalizeIgnoringASCII(text: List[String]): List[String] = {
-    List.empty
+    val transform: PartialFunction[(String, Int), String] = {
+      case (value, 0) => value
+      case (value, _) if isASCIIString(value) => value.toUpperCase
+      case (value, _) => value.toLowerCase
+    }
+    text.zipWithIndex.collect(transform)
   }
 
   /**
@@ -28,7 +33,31 @@ object collectionsTask {
    * HINT: Для всех возможных комбинаций чисел стоит использовать Map
    * **/
   def numbersToNumericString(text: String): String = {
-    ""
+    val textNumbers = Map(
+      "0" -> "zero",
+      "1" -> "one",
+      "2" -> "two",
+      "3" -> "three",
+      "4" -> "four",
+      "5" -> "five",
+      "6" -> "six",
+      "7" -> "seven",
+      "8" -> "eight",
+      "9" -> "nine"
+    )
+    // вариант с заменой по отдельным словам и цифрам
+    val words: Array[String] = text.split("\\b")
+    words.collect {
+      case value if textNumbers.contains(value) => textNumbers(value)
+      case word => word
+    }.mkString
+    // вариант с заменой посимвольно
+    /*
+    text.toCharArray.collect {
+      case ch if ch.isDigit => textNumbers(ch.toString) + " "
+      case ch => ch.toString
+    }.mkString
+    */
   }
 
   /**
@@ -41,12 +70,30 @@ object collectionsTask {
 
   case class Auto(mark: String, model: String)
 
+  case class AutoIgnoreCase(auto: Auto) {
+    private val key: (String, String) =
+      (auto.mark.toLowerCase, auto.model.toLowerCase)
+
+    // кастомный equals в обёртке AutoIgnoreCase, чтобы Auto сравнивался по ключу независимо от регистра значений полей
+    override def equals(obj: Any): Boolean = obj match {
+      case other: AutoIgnoreCase => this.key == other.key
+      case _ => false
+    }
+
+    override def hashCode(): Int = key.hashCode()
+  }
+
   /**
    * Хотим узнать какие машины можно обслужить учитывая этих двух дилеров.
    * Реализуйте метод, который примет две коллекции (два источника) и вернёт объединенный список уникальный значений
    **/
   def intersectionAuto(dealerOne: Iterable[Auto], dealerTwo: Iterable[Auto]): Iterable[Auto] = {
-    Iterable.empty
+    // уникальные машины независимо у каждого дилера (убираются дубликаты, которые могли быть по регистру)
+    val setOne = dealerOne.map(AutoIgnoreCase.apply).toSet
+    val setTwo = dealerTwo.map(AutoIgnoreCase.apply).toSet
+    // строим пересечение двух множеств (только общие элементы), которое уберёт дубликаты между двумя дилерами по AutoIgnoreCase,
+    // и создаём итоговый список результатов, доставая Auto из AutoIgnoreCase
+    (setOne & setTwo).map(_.auto)
   }
 
   /**
@@ -55,6 +102,10 @@ object collectionsTask {
    * и вернёт уникальный список машин обслуживающихся в первом дилерском центре и не обслуживающимся во втором
    **/
   def filterAllLeftDealerAutoWithoutRight(dealerOne: Iterable[Auto], dealerTwo: Iterable[Auto]): Iterable[Auto] = {
-    Iterable.empty
+    val setOne = dealerOne.map(AutoIgnoreCase.apply).toSet
+    val setTwo = dealerTwo.map(AutoIgnoreCase.apply).toSet
+    // по условию задачи: нужны элементы первого множества ("обслуживающихся в первом дилерском центре"), которых
+    // нет во втором ("и не обслуживающимся во втором") - это и есть определение разности множеств (-- или diff, или &~)
+    (setOne -- setTwo).map(_.auto)
   }
 }
