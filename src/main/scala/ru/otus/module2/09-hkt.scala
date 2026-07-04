d723e76f957da27cd291a94a8169ca521110329b
dkochnev <dekochnev@yandex.ru>
2026-07-04 12:41:25 +0300

    homework-05 HKT tupleF with tests +4

--- src/main/scala/ru/otus/module2/09-hkt.scala
+++ src/main/scala/ru/otus/module2/09-hkt.scala
@@ -19,7 +19,7 @@ object higher_kinded_types{
     def map[B](f: A => B): F[B]
     def flatMap[B](f: A => F[B]): F[B]
   }
-  
+
   def tupleBindable[F[_], A, B](fa: Bindable[F, A], fb: Bindable[F, B]): F[(A, B)] =
     fa.flatMap{ a => fb.map((a, _))}
 
@@ -43,10 +43,12 @@ object higher_kinded_types{
 
   val list1 = List(1, 2, 3)
   val list2 = List(4, 5, 6)
-  
+
   val r1 = println(tupleBindable(optBindable(optA), optBindable(optB)))
   val r2 = println(tupleBindable(listBindable(list1), listBindable(list2)))
 
+  // Задача: Использовать на практике концепции занятия - HKT и Implicits и реализовать общий метод tupleF, который
+  // будет способен превратить два любых контейнера с типом F[A] и F[B] в один контейнер с типом F[(A, B)].
 
   // абстракция над HKT F[_] = "для любого F я умею делать map и flatMap"
   trait FBindable[F[_]] {
