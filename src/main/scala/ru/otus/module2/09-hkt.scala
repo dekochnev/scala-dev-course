a82991b1a826b5e7c3c27e377b3c5e8ec3f5b23e
dkochnev <dekochnev@yandex.ru>
2026-07-04 03:22:41 +0300

    homework-05 HKT tupleF with tests

--- src/main/scala/ru/otus/module2/09-hkt.scala
+++ src/main/scala/ru/otus/module2/09-hkt.scala
@@ -12,14 +12,14 @@ object higher_kinded_types{
     a.flatMap{ a => b.map((a, _))}
 
 
-  // tupleF должна превратить два любых контейнера с типом F[A] и F[B] в один контейнер с типом F[(A, B)].
-  def tupleF[F[_], A, B](fa: F[A], fb: F[B]): F[(A, B)] = ???
-  
+//  def tupleF[F[_], A, B](fa: F[A], fb: F[B]): F[(A, B)] = ???
+
+
   trait Bindable[F[_], A] {
     def map[B](f: A => B): F[B]
     def flatMap[B](f: A => F[B]): F[B]
   }
-  
+
   def tupleBindable[F[_], A, B](fa: Bindable[F, A], fb: Bindable[F, B]): F[(A, B)] =
     fa.flatMap{ a => fb.map((a, _))}
 
@@ -30,24 +30,51 @@ object higher_kinded_types{
     override def flatMap[B](f: A => Option[B]): Option[B] = opt.flatMap(f)
   }
 
-  def listBindable[A](opt: List[A]): Bindable[List, A] = new Bindable[List, A] {
-    override def map[B](f: A => B): List[B] = opt.map(f)
+  def listBindable[A](list: List[A]): Bindable[List, A] = new Bindable[List, A] {
+    override def map[B](f: A => B): List[B] = list.map(f)
 
-    override def flatMap[B](f: A => List[B]): List[B] = opt.flatMap(f)
+    override def flatMap[B](f: A => List[B]): List[B] = list.flatMap(f)
   }
-
-
 
   val optA: Option[Int] = Some(1)
   val optB: Option[Int] = Some(2)
 
-  val list1 = List(1, 2, 3)
-  val list2 = List(4, 5, 6)
-  
-  val r1 = println(tupleBindable(optBindable(optA), optBindable(optB)))
-  val r2 = println(tupleBindable(listBindable(list1), listBindable(list2)))
+//  val list1 = List(1, 2, 3)
+//  val list2 = List(4, 5, 6)
+
+    val list1 = List(1, 2)
+    val list2 = List("A", "B")
 
 
+  //  val r1 = println(tupleBindable(optBindable(optA), optBindable(optB)))
+//  val r2 = println(tupleBindable(listBindable(list1), listBindable(list2)))
 
+  // абстракция над HKT F[_] = "для любого F я умею делать map и flatMap"
+  trait FBindable[F[_]] {
+    def map[A, B](fa: F[A])(f: A => B): F[B]
+    def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
+  }
 
+  // Реализуем tupleF через FBindable и заданной где-то реализации через given для конкретного типа (должен работать с ops)
+  def tupleF[F[_], A, B](fa: F[A], fb: F[B])(using ops: FBindable[F]): F[(A, B)] =
+    ops.flatMap(fa) { a =>
+      ops.map(fb)((a, _))
+    }
+
+  given optionFBindable: FBindable[Option] with {
+    override def flatMap[A, B](fa: Option[A])(f: A => Option[B]): Option[B] = fa.flatMap(f)
+
+    override def map[A, B](fa: Option[A])(f: A => B): Option[B] = fa.map(f)
+  }
+
+  given listFBindable: FBindable[List] with {
+    override def flatMap[A, B](fa: List[A])(f: A => List[B]): List[B] = fa.flatMap(f)
+
+    override def map[A, B](fa: List[A])(f: A => B): List[B] = fa.map(f)
+  }
+
+  @main def test1: Unit = {
+    println(tupleBindable(listBindable(list1), listBindable(list2)))
+  }
 }
+
---
+++ src/test/scala/ru/otus/module2/HKTSpec.scala
@@ -0,0 +1,50 @@
+package ru.otus.module2
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import ru.otus.module2.higher_kinded_types.*  //{tupleF, FBindable, given}
+
+class HKTSpec extends AnyFlatSpec with Matchers {
+
+  val optA: Option[Int] = Some(10)
+  val optB: Option[Int] = Some(20)
+  val optNone: Option[Int] = None
+  val listA = List(1, 2)
+  val listB = List("A", "B")
+
+  "tupleF with Option" should "combine two Some values into a Some pair" in {
+    tupleF(optA, optB) shouldBe Some((10, 20))
+  }
+
+  it should "not be equal to a different pair" in {
+    tupleF(optA, optB) should not equal Some((10, 30))
+  }
+
+  it should "return None if the second argument is None" in {
+    tupleF(optA, optNone) shouldBe None
+  }
+
+  it should "return None if the first argument is None" in {
+    tupleF(optNone, optB) shouldBe None
+  }
+
+  "tupleF with List" should "produce all pairs" in {
+    val expected = List(
+      (1, "A"), (1, "B"),
+      (2, "A"), (2, "B")
+    )
+    tupleF(listA, listB) shouldBe expected
+  }
+
+  it should "return an empty list if the first list is empty" in {
+    tupleF(List.empty[Int], listB) shouldBe List.empty
+  }
+
+  it should "return an empty list if the second list is empty" in {
+    tupleF(listA, List.empty[String]) shouldBe List.empty
+  }
+
+  it should "return an empty list if both lists are empty" in {
+    tupleF(List.empty[Int], List.empty[String]) shouldBe List.empty
+  }
+}
